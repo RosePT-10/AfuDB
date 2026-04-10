dafb195103237760f1350d1a0f93c7153fc3b40f
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-09 18:45:57 -0600

    js prog 5: submission process begun

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,18 +12,8 @@ function scroll_to(clicked_link, nav_height) {
 }
 
 
-// mod listings
-function createListing(mod_name, developer_name, supported_game_versions, download_link, description) {
-
-}
-
-function testing(){
-    alert ("heyyo whatsup")
-}
-
-
 jQuery(document).ready(function() {
-	
+
 	/*
 	    Navigation
 	*/
---
+++ submissions.js
@@ -0,0 +1,29 @@
+function displayListing(mod_name, developer_name, supported_game_versions, download_link, description, order) {
+
+}
+
+function testing(){
+    alert (mods)
+}
+
+var mods = [];
+
+mods.push({
+    "mod_name": "AFUSeizurePrevention",
+    "developer_name": "RosePT-10",
+    "supported_game_versions": "v0.26",
+    "download_link": "https://github.com/RosePT-10/AFUSeizurePrevention",
+    "description": "A mod which alters the lighting effect of the Riot Stick and EMP Grenades, as well as visually removing flashbangs entirely, to prevent seizures. ",
+    "order": "0"
+})
+
+mods.push({
+    "mod_name": "AFU: Ultimate Premium Edition",
+    "developer_name": "RosePT-10",
+    "supported_game_versions": "v0.26",
+    "download_link": "https://github.com/RosePT-10/AFU-Ultimate-Premium-Edition",
+    "description": "A mod for Airframe Ultra which makes extensive changes to... pretty much everything. It's AFU Repainted if you know what that means. If you don't, its just replacing the games assets with a bunch of memes.",
+    "order": "1"
+})
+
+testing();
