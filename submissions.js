be7703ac1d8fc12dc8ba613bf746c534bc6984ae
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-10 12:05:42 -0600

    Website now technically fully functional (but still needs polish and decorating)

--- submissions.js
+++ submissions.js
@@ -4,9 +4,23 @@ var mods_length = mods.length;
 function displayListing() {
     // iterate over the entire array and display listings one at a time
     mods.forEach(function (item, index) {
-    //console.log(item, index);
-    let textNode = document.createTextNode(item.mod_name); 
-    document.body.appendChild(textNode);
+        //console.log(item, index);
+        let br = document.createElement('br');
+        let element = document.getElementById('modListings');
+
+        element.innerHTML += `${item.mod_name}`
+        element.appendChild(br);
+        element.innerHTML += `${item.developer_name}`
+        element.appendChild(br);
+        element.innerHTML += `${item.supported_game_versions}`
+        element.appendChild(br);
+        element.innerHTML += `${item.download_link}`
+        element.appendChild(br);
+        element.innerHTML += `${item.description}`
+        element.appendChild(br);
+        element.innerHTML += `${item.order}`
+        element.appendChild(br);
+        element.appendChild(br);    
     });
     
 }
@@ -19,7 +33,7 @@ function testing(){
 mods.push({
     "mod_name": "AFUSeizurePrevention",
     "developer_name": "RosePT-10",
-    "supported_game_versions": ["v0.26"],
+    "supported_game_versions": ["0.12", "0.152", "0.17", "0.26", "0.34"],
     "download_link": "https://github.com/RosePT-10/AFUSeizurePrevention",
     "description": "A mod which alters the lighting effect of the Riot Stick and EMP Grenades, as well as visually removing flashbangs entirely, to prevent seizures. ",
     "order": "0"
@@ -42,6 +56,3 @@ mods.push({
     "description": "Timer mod for Airframe Ultra, for racing against yourself on the track! Adds an in-game timer that automatically starts/stops when races begin/end. Keeps track of your best times for each map, and each bike on each map! Individual 'splits' as well as your fastest pace finishing each race Works for beta 2 and 3, don't know about beta 1",
     "order": "2"
 })
-
-//displayListing();
-//testing();
