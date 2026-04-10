20315e6019252c4a2223153040bd2c3039671edb
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-09 18:16:45 -0600

    figuring out javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,8 +12,19 @@ function scroll_to(clicked_link, nav_height) {
 }
 
 
+
+
+
 jQuery(document).ready(function() {
-	
+
+	// mod listings
+	function createListing(mod_name, developer_name, supported_game_versions, download_link, description) {
+
+	}
+	function testing(){
+		alert ("heyyo whatsup")
+	}
+
 	/*
 	    Navigation
 	*/
