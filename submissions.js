1dfc7e5d77f7914af0045c6360ca3df8827b8f0d
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-09 19:13:03 -0600

    js prog 10: pls just display the mod name

--- submissions.js
+++ submissions.js
@@ -6,6 +6,7 @@ function displayListing() {
     for (var i = 0; i < mods_length; i++){
         var current_mod = mods.at(i);
         console.log(current_mod.mod_name);
+        alert (current_mod.mod_name);
     }
     
 }
@@ -18,7 +19,7 @@ function testing(){
 mods.push({
     "mod_name": "AFUSeizurePrevention",
     "developer_name": "RosePT-10",
-    "supported_game_versions": "v0.26",
+    "supported_game_versions": ["v0.26"],
     "download_link": "https://github.com/RosePT-10/AFUSeizurePrevention",
     "description": "A mod which alters the lighting effect of the Riot Stick and EMP Grenades, as well as visually removing flashbangs entirely, to prevent seizures. ",
     "order": "0"
@@ -27,7 +28,7 @@ mods.push({
 mods.push({
     "mod_name": "AFU: Ultimate Premium Edition",
     "developer_name": "RosePT-10",
-    "supported_game_versions": "v0.26",
+    "supported_game_versions": ["v0.26"],
     "download_link": "https://github.com/RosePT-10/AFU-Ultimate-Premium-Edition",
     "description": "A mod for Airframe Ultra which makes extensive changes to... pretty much everything. It's AFU Repainted if you know what that means. If you don't, its just replacing the games assets with a bunch of memes.",
     "order": "1"
@@ -36,11 +37,11 @@ mods.push({
 mods.push({
     "mod_name": "TimerMod",
     "developer_name": "Knight-Ragu",
-    "supported_game_versions": "v0.26",
+    "supported_game_versions": ["v0.26"],
     "download_link": "https://github.com/Knight-Ragu/TimerMod",
     "description": "Timer mod for Airframe Ultra, for racing against yourself on the track! Adds an in-game timer that automatically starts/stops when races begin/end. Keeps track of your best times for each map, and each bike on each map! Individual 'splits' as well as your fastest pace finishing each race Works for beta 2 and 3, don't know about beta 1",
     "order": "2"
 })
 
 displayListing();
-testing();
+//testing();
