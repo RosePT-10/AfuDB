79a3f0f5a2aab61ec7a9ea9e4fe56d33996dc5de
Knight-Ragu <knightragu@gmail.com>
2026-04-11 11:14:35 -0400

    Submit TimerMod

--- submissions.js
+++ submissions.js
@@ -88,3 +88,11 @@ mods.push({
     "download_link": "https://github.com/RosePT-10/UPEAssetSwapper",
     "description": "Melon Loader mod to replace assets in the game Airframe Ultra. Was created for use in Aiframe Ulta: Ultimate Premium Edition (UPE), a mod which replaces game assets with memes. Currently it is non functional. I started my Melon Loader modding journey with this but hit a wall and couldn't figure it out. I then got UPEAddons working and that was a huge success. I plan to come back to this tool at a later time. Until this mod has a release posted, assume that it doesnt do anything."
 })
+
+mods.push({
+    "mod_name": "Timer Mod",
+    "developer_name": "Knight-Ragu",
+    "supported_game_versions": ["Beta 1", "Beta 2", "Beta 3"],
+    "download_link": "https://github.com/Knight-Ragu/TimerMod",
+    "description": "Timer mod for Airframe Ultra, for racing against yourself on the track!",
+})
