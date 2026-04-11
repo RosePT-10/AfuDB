87df16163568ce0d944cf84bc5335cc24f82b4dc
Tal-01 <116579574+Tal-01@users.noreply.github.com>
2026-04-10 21:22:14 -0500

    Update submissions.js

--- submissions.js
+++ submissions.js
@@ -91,3 +91,11 @@ mods.push({
     "description": "Melon Loader mod to replace assets in the game Airframe Ultra. Was created for use in Aiframe Ulta: Ultimate Premium Edition (UPE), a mod which replaces game assets with memes. Currently it is non functional. I started my Melon Loader modding journey with this but hit a wall and couldn't figure it out. I then got UPEAddons working and that was a huge success. I plan to come back to this tool at a later time. Until this mod has a release posted, assume that it doesnt do anything.",
     "order": "4"
 })
+mods.push({
+    "mod_name": "UPEAssetSwapper",
+    "developer_name": "RosePT-10",
+    "supported_game_versions": ["Beta 4"],
+    "download_link": "https://github.com/RosePT-10/UPEAssetSwapper",
+    "description": "Melon Loader mod to replace assets in the game Airframe Ultra. Was created for use in Aiframe Ulta: Ultimate Premium Edition (UPE), a mod which replaces game assets with memes. Currently it is non functional. I started my Melon Loader modding journey with this but hit a wall and couldn't figure it out. I then got UPEAddons working and that was a huge success. I plan to come back to this tool at a later time. Until this mod has a release posted, assume that it doesnt do anything.",
+    "order": "5"
+})
